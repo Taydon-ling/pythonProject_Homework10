f60c6a8bc238ee44528b8dcb05f202da2932f395
Taydon <2889837@student.dublinisc.com>
2024-10-25 15:27:21 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+
+print("Use the quadratic formula to calculate x**2 + 2x - 5 = 0")
+
+# The answer of the first root
+print((-2 - (2**2 - 4*1*(-5))**0.5)/(2*1))
+
+# The answer of the second root
+print((-2 + (2**2 - 4*1*(-5))**0.5)/(2*1))
+
+print("That are the answers.")
+
